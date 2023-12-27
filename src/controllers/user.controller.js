98af57fae03a414f9fa95c0e06a963d2e1764d4a
Ayush Tiwar <ayush.tiwari0031@gmail.com>
2023-12-27 17:32:03 +0530

    testing

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -16,15 +16,14 @@ const registerUser=asyncHandler(async (req,res)=>{
    //return res
 
    const  {fullName,email,username,password}=req.body
-   console.log("email: ",email);
-
+   
    if([fullName,email,username,password].some((field)=>
    field?.trim()==="")
    ){
     throw new ApiError(400,"All fields are compulsory")
    }
 
-   const existedUser=User.findOne({
+   const existedUser=await User.findOne({
     $or:[{ email },{ username }]
    })
 
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -2,83 +2,81 @@ import mongoose, {Schema} from "mongoose";
 import jwt from "jsonwebtoken"
 import bcrypt from "bcrypt"
 
-const userSchema=new Schema(
+const userSchema = new Schema(
     {
-       username:{
-        type:String,
-        required:true,
-        unique:true,
-        lowercase:true,
-        trim:true,
-        index:true
-       },
-       email: {
-        type: String,
-        required: true,
-        unique: true,
-        lowecase: true,
-        trim: true, 
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true, 
+            index: true
         },
-       fullName: {
-         type: String,
-         required: true,
-         trim: true, 
-         index: true
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowecase: true,
+            trim: true, 
         },
-       avatar: {
-         type: String, // cloudinary url
-         required: true,
+        fullName: {
+            type: String,
+            required: true,
+            trim: true, 
+            index: true
         },
-       coverImage: {
-         type: String, // cloudinary url
+        avatar: {
+            type: String, // cloudinary url
+            required: true,
         },
-       watchHistory: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: "Video"
-        }
+        coverImage: {
+            type: String, // cloudinary url
+        },
+        watchHistory: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Video"
+            }
         ],
-       password: {
-         type: String,
-         required: [true, 'Password is required']
+        password: {
+            type: String,
+            required: [true, 'Password is required']
         },
-       refreshToken: {
-         type: String
+        refreshToken: {
+            type: String
         }
 
     },
     {
-        timestamps:true
+        timestamps: true
     }
-    
 )
 
-userSchema.pre("save",async function(next){
+userSchema.pre("save", async function (next) {
     if(!this.isModified("password")) return next();
-    this.password=await bcrypt.hash(this.password,10)
+
+    this.password = await bcrypt.hash(this.password, 10)
     next()
 })
 
-userSchema.methods.isPasswordCorrect=async function(password){
-    return await bcrypt.compare(password,this.password)
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password)
 }
 
-userSchema.methods.generateAccessToken=function()
-{
+userSchema.methods.generateAccessToken = function(){
     return jwt.sign(
         {
-            _id:this._id,
-            email:this.email,
-            username:this.username,
-            fullName:this.fullName
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName: this.fullName
         },
         process.env.ACCESS_TOKEN_SECRET,
         {
-            expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
         }
     )
 }
-
 userSchema.methods.generateRefreshToken = function(){
     return jwt.sign(
         {
@@ -92,4 +90,4 @@ userSchema.methods.generateRefreshToken = function(){
     )
 }
 
-export const User=mongoose.model("User",userSchema)
+export const User = mongoose.model("User", userSchema)
